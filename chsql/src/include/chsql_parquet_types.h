abe9e4b2b4954c796144237a4595432df5949083
akvlad <akvlad90@gmail.com>
2025-03-14 18:11:09 +0200

    WIP

--- chsql/src/include/chsql_parquet_types.h
+++ chsql/src/include/chsql_parquet_types.h
@@ -11,17 +11,37 @@
 struct ParquetType {
   /*duckdb_parquet::ConvertedType::type -> replaced to int to support -1 nodata  value*/
   int converted_type;
-  duckdb_parquet::Type::type parquet_type;
-  const duckdb::LogicalType &logical_type;
-  ParquetType(int converted_type, duckdb_parquet::Type::type parquet_type,
-              const duckdb::LogicalType &logical_type)
+  /* duckdb_parquet::Type::type -> replaced to int to support -1 for no matter value */
+  int parquet_type;
+  const duckdb::LogicalType logical_type;
+  ParquetType(int converted_type, int parquet_type, const duckdb::LogicalType &logical_type)
       : converted_type(converted_type), parquet_type(parquet_type), logical_type(logical_type) {}
-  bool check_type(const duckdb::vector<duckdb_parquet::SchemaElement> &schema, idx_t idx);
+  virtual bool check_type(const duckdb::vector<duckdb_parquet::SchemaElement> &schema, idx_t idx);
+  virtual duckdb::LogicalType get_logical_type(const duckdb_parquet::SchemaElement &schema);
+};
+
+struct LogicalParquetType : public ParquetType {
+  bool (*get_isset)(const duckdb_parquet::SchemaElement& el);
+
+  LogicalParquetType(bool (*get_isset) (const duckdb_parquet::SchemaElement& el),
+    const duckdb::LogicalType& logical_type)
+  : ParquetType(-1, duckdb_parquet::Type::type::INT32, logical_type), get_isset(get_isset) {}
+  bool check_type(const duckdb::vector<duckdb_parquet::SchemaElement> &schema, idx_t idx) override;
+};
+
+struct JSONParquetType : public ParquetType {
+  JSONParquetType(): ParquetType(duckdb_parquet::ConvertedType::JSON, -1, duckdb::LogicalType::SQLNULL) {}
+  duckdb::LogicalType get_logical_type(const duckdb_parquet::SchemaElement &schema) override;
+};
+
+struct DecimalParquetType : public ParquetType {
+  DecimalParquetType(): ParquetType(-1, duckdb_parquet::Type::type::INT32, duckdb::LogicalType::SQLNULL) {}
+  bool check_type(const duckdb::vector<duckdb_parquet::SchemaElement> &schema, idx_t idx) override;
+  duckdb::LogicalType get_logical_type(const duckdb_parquet::SchemaElement &schema) override;
 };
 
 class ParquetTypesManager {
   protected:
-    std::vector<ParquetType> types;
     static ParquetTypesManager *instance;
     static std::mutex instance_mutex;
     ParquetTypesManager();
--- chsql/src/parquet_types.cpp
+++ chsql/src/parquet_types.cpp
@@ -1,242 +1,139 @@
 #include "chsql_parquet_types.h"
 
 bool ParquetType::check_type(const duckdb::vector<duckdb_parquet::SchemaElement> &schema, idx_t idx) {
-  auto &el = schema[idx];
-  if (el.type != parquet_type) {
-    return false;
-  }
-  if (converted_type == -1) {
-    return !el.__isset.converted_type;
-  }
-  return int(el.converted_type) == converted_type;
+	auto &el = schema[idx];
+	if (parquet_type >= 0 && int(el.type) != parquet_type) {
+		return false;
+	}
+	if (converted_type == -1) {
+		return !el.__isset.converted_type;
+	}
+	return int(el.converted_type) == converted_type;
+};
+
+duckdb::LogicalType ParquetType::get_logical_type(const duckdb_parquet::SchemaElement &schema) {
+	return logical_type;
+}
+
+bool LogicalParquetType::check_type(const duckdb::vector<duckdb_parquet::SchemaElement> &schema, idx_t idx) {
+	auto &el = schema[idx];
+	return el.__isset.logicalType && this->get_isset(el);
+}
+
+duckdb::LogicalType JSONParquetType::get_logical_type(const duckdb_parquet::SchemaElement &schema) {
+	return duckdb::LogicalType::JSON();
+}
+
+bool DecimalParquetType::check_type(const duckdb::vector<duckdb_parquet::SchemaElement> &schema, idx_t idx) {
+	auto &el = schema[idx];
+	return el.__isset.converted_type && el.converted_type == duckdb_parquet::ConvertedType::DECIMAL &&
+	       el.__isset.precision && el.__isset.scale && (el.precision > duckdb::DecimalType::MaxWidth() ||
+	                                                    el.type == duckdb_parquet::Type::BYTE_ARRAY ||
+	                                                    el.type == duckdb_parquet::Type::FIXED_LEN_BYTE_ARRAY ||
+	                                                    el.type == duckdb_parquet::Type::INT32 ||
+	                                                    el.type == duckdb_parquet::Type::INT64);
+}
+
+duckdb::LogicalType DecimalParquetType::get_logical_type(const duckdb_parquet::SchemaElement &el) {
+	if (el.precision > duckdb::DecimalType::MaxWidth()) {
+		return duckdb::LogicalType::DOUBLE;
+	}
+	return duckdb::LogicalType::DECIMAL(el.precision, el.scale);
+}
+
+bool isUUID(const duckdb_parquet::SchemaElement &el) {
+	return el.logicalType.__isset.UUID;
+}
+
+bool isTimestampTZ(const duckdb_parquet::SchemaElement &el) {
+	return el.logicalType.__isset.TIMESTAMP && el.logicalType.TIMESTAMP.isAdjustedToUTC;
+}
+
+bool isTimestampNS(const duckdb_parquet::SchemaElement &el) {
+	return el.logicalType.__isset.TIMESTAMP && el.logicalType.TIMESTAMP.unit.__isset.NANOS;
+}
+
+bool isTimestamp(const duckdb_parquet::SchemaElement &el) {
+	return el.logicalType.__isset.TIMESTAMP;
+}
+
+bool isTimeTZ(const duckdb_parquet::SchemaElement &el) {
+	return el.logicalType.__isset.TIME && el.logicalType.TIME.isAdjustedToUTC;
+}
+
+bool isTime(const duckdb_parquet::SchemaElement &el) {
+	return el.logicalType.__isset.TIME;
+}
+
+ParquetType *_types[] = {
+	new LogicalParquetType(isUUID, duckdb::LogicalType::UUID),
+	new LogicalParquetType(isTimestampTZ, duckdb::LogicalType::TIMESTAMP_TZ),
+	new LogicalParquetType(isTimestampNS, duckdb::LogicalType::TIMESTAMP_NS),
+	new LogicalParquetType(isTimestamp, duckdb::LogicalType::TIMESTAMP),
+	new LogicalParquetType(isTimeTZ, duckdb::LogicalType::TIME),
+	new LogicalParquetType(isTime, duckdb::LogicalType::TIME),
+
+	new ParquetType(24, -1, duckdb::LogicalType::SQLNULL),
+	new ParquetType(duckdb_parquet::ConvertedType::INT_8, duckdb_parquet::Type::INT32, duckdb::LogicalType::TINYINT),
+	new ParquetType(duckdb_parquet::ConvertedType::INT_16, duckdb_parquet::Type::INT32, duckdb::LogicalType::SMALLINT),
+	new ParquetType(duckdb_parquet::ConvertedType::INT_32, duckdb_parquet::Type::INT32, duckdb::LogicalType::INTEGER),
+	new ParquetType(duckdb_parquet::ConvertedType::INT_64, duckdb_parquet::Type::INT64, duckdb::LogicalType::BIGINT),
+	new ParquetType(duckdb_parquet::ConvertedType::UINT_8, duckdb_parquet::Type::INT32, duckdb::LogicalType::UTINYINT),
+	new ParquetType(duckdb_parquet::ConvertedType::UINT_16, duckdb_parquet::Type::INT32,
+	                duckdb::LogicalType::USMALLINT),
+	new ParquetType(duckdb_parquet::ConvertedType::UINT_32, duckdb_parquet::Type::INT32, duckdb::LogicalType::UINTEGER),
+	new ParquetType(duckdb_parquet::ConvertedType::UINT_64, duckdb_parquet::Type::INT64, duckdb::LogicalType::UBIGINT),
+	new ParquetType(duckdb_parquet::ConvertedType::DATE, duckdb_parquet::Type::INT32, duckdb::LogicalType::DATE),
+	new ParquetType(duckdb_parquet::ConvertedType::TIME_MICROS, duckdb_parquet::Type::INT64, duckdb::LogicalType::TIME),
+	new ParquetType(duckdb_parquet::ConvertedType::TIME_MILLIS, duckdb_parquet::Type::INT64, duckdb::LogicalType::TIME),
+	new ParquetType(duckdb_parquet::ConvertedType::TIMESTAMP_MILLIS, duckdb_parquet::Type::INT32,
+	                duckdb::LogicalType::TIME),
+	new ParquetType(duckdb_parquet::ConvertedType::TIMESTAMP_MICROS, duckdb_parquet::Type::INT64,
+	                duckdb::LogicalType::TIME),
+	new ParquetType(duckdb_parquet::ConvertedType::INTERVAL, -1, duckdb::LogicalType::INTERVAL),
+	new ParquetType(duckdb_parquet::ConvertedType::UTF8, duckdb_parquet::Type::BYTE_ARRAY,
+	                duckdb::LogicalType::VARCHAR),
+	new ParquetType(duckdb_parquet::ConvertedType::ENUM, duckdb_parquet::Type::BYTE_ARRAY,
+	                duckdb::LogicalType::VARCHAR),
+	new ParquetType(duckdb_parquet::ConvertedType::UTF8, duckdb_parquet::Type::FIXED_LEN_BYTE_ARRAY,
+	                duckdb::LogicalType::VARCHAR),
+	new ParquetType(duckdb_parquet::ConvertedType::ENUM, duckdb_parquet::Type::FIXED_LEN_BYTE_ARRAY,
+	                duckdb::LogicalType::VARCHAR),
+
+	new JSONParquetType(),
+	new DecimalParquetType(),
+
+	new ParquetType(-1, duckdb_parquet::Type::BOOLEAN, duckdb::LogicalType::BOOLEAN),
+	new ParquetType(-1, duckdb_parquet::Type::BOOLEAN, duckdb::LogicalType::BOOLEAN),
+	new ParquetType(-1, duckdb_parquet::Type::INT32, duckdb::LogicalType::INTEGER),
+	new ParquetType(-1, duckdb_parquet::Type::INT64, duckdb::LogicalType::BIGINT),
+	new ParquetType(-1, duckdb_parquet::Type::INT96, duckdb::LogicalType::TIMESTAMP),
+	new ParquetType(-1, duckdb_parquet::Type::FLOAT, duckdb::LogicalType::FLOAT),
+	new ParquetType(-1, duckdb_parquet::Type::DOUBLE, duckdb::LogicalType::DOUBLE),
+	new ParquetType(-1, duckdb_parquet::Type::BYTE_ARRAY, duckdb::LogicalType::BLOB),
+	new ParquetType(-1, duckdb_parquet::Type::FIXED_LEN_BYTE_ARRAY, duckdb::LogicalType::BLOB),
 };
 
 ParquetTypesManager::ParquetTypesManager() {
-    /*UTF8 = 0,
-     MAP = 1, -> no support
-    MAP_KEY_VALUE = 2, -> no support
-    LIST = 3, -> no support
-    ENUM = 4, -> no support
-    DECIMAL = 5, -> no support
-    DATE = 6,
-    TIME_MILLIS = 7
-    TIME_MICROS = 8
-    TIMESTAMP_MILLIS = 9,
-    TIMESTAMP_MICROS = 10,
-    UINT_8 = 11,
-    UINT_16 = 12,
-    UINT_32 = 13,
-    UINT_64 = 14,
-    INT_8 = 15,
-    INT_16 = 16,
-    INT_32 = 17,
-    INT_64 = 18,
-    JSON = 19, -> no support
-    BSON = 20, -> no support
-    INTERVAL = 21 -> no support
-  */
-  types.push_back(ParquetType(duckdb_parquet::ConvertedType::UTF8, duckdb_parquet::Type::BYTE_ARRAY, duckdb::LogicalType::VARCHAR));
-  types.push_back(ParquetType(duckdb_parquet::ConvertedType::DATE, duckdb_parquet::Type::INT32, duckdb::LogicalType::DATE));
-  types.push_back(ParquetType(duckdb_parquet::ConvertedType::TIME_MILLIS, duckdb_parquet::Type::INT32, duckdb::LogicalType::TIME));
-  types.push_back(ParquetType(duckdb_parquet::ConvertedType::TIME_MICROS, duckdb_parquet::Type::INT64, duckdb::LogicalType::TIME));
-  types.push_back(ParquetType(duckdb_parquet::ConvertedType::TIMESTAMP_MILLIS, duckdb_parquet::Type::INT64, duckdb::LogicalType::TIMESTAMP));
-  types.push_back(ParquetType(duckdb_parquet::ConvertedType::TIMESTAMP_MICROS, duckdb_parquet::Type::INT64, duckdb::LogicalType::TIMESTAMP));
-  types.push_back(ParquetType(duckdb_parquet::ConvertedType::UINT_8, duckdb_parquet::Type::INT32, duckdb::LogicalType::UTINYINT));
-  types.push_back(ParquetType(duckdb_parquet::ConvertedType::UINT_16, duckdb_parquet::Type::INT32, duckdb::LogicalType::USMALLINT));
-  types.push_back(ParquetType(duckdb_parquet::ConvertedType::UINT_32, duckdb_parquet::Type::INT32, duckdb::LogicalType::UINTEGER));
-  types.push_back(ParquetType(duckdb_parquet::ConvertedType::UINT_64, duckdb_parquet::Type::INT64, duckdb::LogicalType::UBIGINT));
-  types.push_back(ParquetType(duckdb_parquet::ConvertedType::INT_8, duckdb_parquet::Type::INT32, duckdb::LogicalType::TINYINT));
-  types.push_back(ParquetType(duckdb_parquet::ConvertedType::INT_16, duckdb_parquet::Type::INT32, duckdb::LogicalType::SMALLINT));
-  types.push_back(ParquetType(duckdb_parquet::ConvertedType::INT_32, duckdb_parquet::Type::INT32, duckdb::LogicalType::INTEGER));
-  types.push_back(ParquetType(duckdb_parquet::ConvertedType::INT_64, duckdb_parquet::Type::INT64, duckdb::LogicalType::BIGINT));
 };
 
 ParquetTypesManager *ParquetTypesManager::instance = nullptr;
 std::mutex ParquetTypesManager::instance_mutex;
 
 ParquetTypesManager *ParquetTypesManager::get_instance() {
-  std::lock_guard<std::mutex> lock(instance_mutex);
-  if (instance == nullptr) {
-    instance = new ParquetTypesManager();
-  }
-  return instance;
+	std::lock_guard<std::mutex> lock(instance_mutex);
+	if (instance == nullptr) {
+		instance = new ParquetTypesManager();
+	}
+	return instance;
 }
 
-duckdb::LogicalType ParquetTypesManager::get_logical_type(const duckdb::vector<duckdb_parquet::SchemaElement> &schema, idx_t idx) {
-  auto inst = get_instance();
-  return inst->derive_logical_type(schema[idx], false);
-  /*for (auto it = inst->types.begin(); it != inst->types.end(); ++it) {
-    if (it->check_type(schema, idx)) {
-      return it->logical_type;
-    }
-  }
-  throw std::runtime_error("Unsupported Parquet type");*/
-}
-
-duckdb::LogicalType ParquetTypesManager::derive_logical_type(const duckdb_parquet::SchemaElement &s_ele, bool binary_as_string) {
-	// inner node
-	if (s_ele.type == duckdb_parquet::Type::FIXED_LEN_BYTE_ARRAY && !s_ele.__isset.type_length) {
-		throw duckdb::IOException("FIXED_LEN_BYTE_ARRAY requires length to be set");
-	}
-	if (s_ele.__isset.logicalType) {
-		if (s_ele.logicalType.__isset.UUID) {
-			if (s_ele.type == duckdb_parquet::Type::FIXED_LEN_BYTE_ARRAY) {
-				return duckdb::LogicalType::UUID;
-			}
-		} else if (s_ele.logicalType.__isset.TIMESTAMP) {
-			if (s_ele.logicalType.TIMESTAMP.isAdjustedToUTC) {
-				return duckdb::LogicalType::TIMESTAMP_TZ;
-			} else if (s_ele.logicalType.TIMESTAMP.unit.__isset.NANOS) {
-				return duckdb::LogicalType::TIMESTAMP_NS;
-			}
-			return duckdb::LogicalType::TIMESTAMP;
-		} else if (s_ele.logicalType.__isset.TIME) {
-			if (s_ele.logicalType.TIME.isAdjustedToUTC) {
-				return duckdb::LogicalType::TIME_TZ;
-			}
-			return duckdb::LogicalType::TIME;
+duckdb::LogicalType ParquetTypesManager::get_logical_type(const duckdb::vector<duckdb_parquet::SchemaElement> &schema,
+                                                          idx_t idx) {
+	for (auto &type: _types) {
+		if (type->check_type(schema, idx)) {
+			return type->get_logical_type(schema[idx]);
 		}
 	}
-	if (s_ele.__isset.converted_type) {
-		// Legacy NULL type, does no longer exist, but files are still around of course
-		if (static_cast<uint8_t>(s_ele.converted_type) == 24) {
-			return duckdb::LogicalTypeId::SQLNULL;
-		}
-		switch (s_ele.converted_type) {
-		case duckdb_parquet::ConvertedType::INT_8:
-			if (s_ele.type == duckdb_parquet::Type::INT32) {
-				return duckdb::LogicalType::TINYINT;
-			} else {
-				throw duckdb::IOException("INT8 converted type can only be set for value of Type::INT32");
-			}
-		case duckdb_parquet::ConvertedType::INT_16:
-			if (s_ele.type == duckdb_parquet::Type::INT32) {
-				return duckdb::LogicalType::SMALLINT;
-			} else {
-				throw duckdb::IOException("INT16 converted type can only be set for value of Type::INT32");
-			}
-		case duckdb_parquet::ConvertedType::INT_32:
-			if (s_ele.type == duckdb_parquet::Type::INT32) {
-				return duckdb::LogicalType::INTEGER;
-			} else {
-				throw duckdb::IOException("INT32 converted type can only be set for value of Type::INT32");
-			}
-		case duckdb_parquet::ConvertedType::INT_64:
-			if (s_ele.type == duckdb_parquet::Type::INT64) {
-				return duckdb::LogicalType::BIGINT;
-			} else {
-				throw duckdb::IOException("INT64 converted type can only be set for value of Type::INT32");
-			}
-		case duckdb_parquet::ConvertedType::UINT_8:
-			if (s_ele.type == duckdb_parquet::Type::INT32) {
-				return duckdb::LogicalType::UTINYINT;
-			} else {
-				throw duckdb::IOException("UINT8 converted type can only be set for value of Type::INT32");
-			}
-		case duckdb_parquet::ConvertedType::UINT_16:
-			if (s_ele.type == duckdb_parquet::Type::INT32) {
-				return duckdb::LogicalType::USMALLINT;
-			} else {
-				throw duckdb::IOException("UINT16 converted type can only be set for value of Type::INT32");
-			}
-		case duckdb_parquet::ConvertedType::UINT_32:
-			if (s_ele.type == duckdb_parquet::Type::INT32) {
-				return duckdb::LogicalType::UINTEGER;
-			} else {
-				throw duckdb::IOException("UINT32 converted type can only be set for value of Type::INT32");
-			}
-		case duckdb_parquet::ConvertedType::UINT_64:
-			if (s_ele.type == duckdb_parquet::Type::INT64) {
-				return duckdb::LogicalType::UBIGINT;
-			} else {
-				throw duckdb::IOException("UINT64 converted type can only be set for value of Type::INT64");
-			}
-		case duckdb_parquet::ConvertedType::DATE:
-			if (s_ele.type == duckdb_parquet::Type::INT32) {
-				return duckdb::LogicalType::DATE;
-			} else {
-				throw duckdb::IOException("DATE converted type can only be set for value of Type::INT32");
-			}
-		case duckdb_parquet::ConvertedType::TIMESTAMP_MICROS:
-		case duckdb_parquet::ConvertedType::TIMESTAMP_MILLIS:
-			if (s_ele.type == duckdb_parquet::Type::INT64) {
-				return duckdb::LogicalType::TIMESTAMP;
-			} else {
-				throw duckdb::IOException("TIMESTAMP converted type can only be set for value of Type::INT64");
-			}
-		case duckdb_parquet::ConvertedType::DECIMAL:
-			if (!s_ele.__isset.precision || !s_ele.__isset.scale) {
-				throw duckdb::IOException("DECIMAL requires a length and scale specifier!");
-			}
-			if (s_ele.precision > duckdb::DecimalType::MaxWidth()) {
-				return duckdb::LogicalType::DOUBLE;
-			}
-			switch (s_ele.type) {
-			case duckdb_parquet::Type::BYTE_ARRAY:
-			case duckdb_parquet::Type::FIXED_LEN_BYTE_ARRAY:
-			case duckdb_parquet::Type::INT32:
-			case duckdb_parquet::Type::INT64:
-				return duckdb::LogicalType::DECIMAL(s_ele.precision, s_ele.scale);
-			default:
-				throw duckdb::IOException(
-				    "DECIMAL converted type can only be set for value of Type::(FIXED_LEN_)BYTE_ARRAY/INT32/INT64");
-			}
-		case duckdb_parquet::ConvertedType::UTF8:
-		case duckdb_parquet::ConvertedType::ENUM:
-			switch (s_ele.type) {
-			case duckdb_parquet::Type::BYTE_ARRAY:
-			case duckdb_parquet::Type::FIXED_LEN_BYTE_ARRAY:
-				return duckdb::LogicalType::VARCHAR;
-			default:
-				throw duckdb::IOException("UTF8 converted type can only be set for Type::(FIXED_LEN_)BYTE_ARRAY");
-			}
-		case duckdb_parquet::ConvertedType::TIME_MILLIS:
-			if (s_ele.type == duckdb_parquet::Type::INT32) {
-				return duckdb::LogicalType::TIME;
-			} else {
-				throw duckdb::IOException("TIME_MILLIS converted type can only be set for value of Type::INT32");
-			}
-		case duckdb_parquet::ConvertedType::TIME_MICROS:
-			if (s_ele.type == duckdb_parquet::Type::INT64) {
-				return duckdb::LogicalType::TIME;
-			} else {
-				throw duckdb::IOException("TIME_MICROS converted type can only be set for value of Type::INT64");
-			}
-		case duckdb_parquet::ConvertedType::INTERVAL:
-			return duckdb::LogicalType::INTERVAL;
-		case duckdb_parquet::ConvertedType::JSON:
-			return duckdb::LogicalType::JSON();
-		case duckdb_parquet::ConvertedType::MAP:
-		case duckdb_parquet::ConvertedType::MAP_KEY_VALUE:
-		case duckdb_parquet::ConvertedType::LIST:
-		case duckdb_parquet::ConvertedType::BSON:
-		default:
-			throw duckdb::IOException("Unsupported converted type (%d)", (int32_t)s_ele.converted_type);
-		}
-	} else {
-		// no converted type set
-		// use default type for each physical type
-		switch (s_ele.type) {
-		case duckdb_parquet::Type::BOOLEAN:
-			return duckdb::LogicalType::BOOLEAN;
-		case duckdb_parquet::Type::INT32:
-			return duckdb::LogicalType::INTEGER;
-		case duckdb_parquet::Type::INT64:
-			return duckdb::LogicalType::BIGINT;
-		case duckdb_parquet::Type::INT96: // always a timestamp it would seem
-			return duckdb::LogicalType::TIMESTAMP;
-		case duckdb_parquet::Type::FLOAT:
-			return duckdb::LogicalType::FLOAT;
-		case duckdb_parquet::Type::DOUBLE:
-			return duckdb::LogicalType::DOUBLE;
-		case duckdb_parquet::Type::BYTE_ARRAY:
-		case duckdb_parquet::Type::FIXED_LEN_BYTE_ARRAY:
-			if (binary_as_string) {
-				return duckdb::LogicalType::VARCHAR;
-			}
-			return duckdb::LogicalType::BLOB;
-		default:
-			return duckdb::LogicalType::INVALID;
-		}
-	}
+	throw std::runtime_error("Unsupported Parquet type");
 }
